c84c1bcdfbac5fedf27d0edba7a4f28e443dde7c
Andrew Snedeker <49925079+ansneds@users.noreply.github.com>
2019-04-29 17:38:11 -0500

    DVDType additions

--- TeamProject/src/DVDType.java
+++ TeamProject/src/DVDType.java
@@ -1,4 +1,63 @@
 
 public class DVDType {
+	private String movieName;
+	private String starName;
+	private String producerName;
+	private String directorName;
+	private String productionCompany;
+	private int copies;
+	
+	public DVDType(String _movieName, String _starName, String _producerName, String _directorName, String _productionCompany, int _copies) {
+		movieName = _movieName;
+		starName = _starName;
+		producerName = _producerName;
+		directorName = _directorName;
+		productionCompany = _productionCompany;
+		copies = _copies;
+	}
+	
+	public DVDType() {
+		movieName = "empty";
+		starName = "empty";
+		producerName = "empty";
+		directorName = "empty";
+		productionCompany = "empty";
+		copies = -1;
+	}
 
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+	
+	
+
+	public void setStarName(String starName) {
+		this.starName = starName;
+	}
+
+	public void setProducerName(String producerName) {
+		this.producerName = producerName;
+	}
+
+	public void setDirectorName(String directorName) {
+		this.directorName = directorName;
+	}
+
+	public void setProductionCompany(String productionCompany) {
+		this.productionCompany = productionCompany;
+	}
+
+	public int getCopies() {
+		return copies;
+	}
+
+	public void setCopies(int copies) {
+		this.copies = copies;
+	}
+
+	public String toString() {
+		return "Movie: " + movieName + "\nStarring: " + starName + "\nProducer: " + producerName
+				+ "\nDirector: " + directorName + "\nProduction Company: " + productionCompany + "\n# of copies: " + copies;
+	}
+	
 }
--- TeamProject/src/DriverMain.java
+++ TeamProject/src/DriverMain.java
@@ -2,7 +2,8 @@
 public class DriverMain {
 
 	public static void main(String[] args) {
-		
+		DVDType test = new DVDType("The Fast and the Furious", "Paul Walker", );
+		System.out.println(test);
 		
 	}
 
