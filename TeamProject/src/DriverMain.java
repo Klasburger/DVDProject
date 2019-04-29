81c057633528d76361962035779f5e9da49e5d8e
Klasburger <matthewklas@gmail.com>
2019-04-29 18:01:05 -0500

    Update DriverMain.java

--- TeamProject/src/DriverMain.java
+++ TeamProject/src/DriverMain.java
@@ -2,20 +2,13 @@ import java.io.FileNotFoundException;
 
 public class DriverMain {
 
-<<<<<<< HEAD
+
 	public static void main(String[] args) throws FileNotFoundException {
 		//test toString()
 		DVDType test = new DVDType("The Fast and the Furious", "Paul Walker", "Neal H. Moritz", "Rob Cohen", "Universal Pictures", 5);
 		System.out.println(test);
-=======
-	public static void main(String[] args) {
-		
->>>>>>> aec5c7470c37b2534a8e8f1c8880fd4570390d74
-		
-		DVDList dvdlist= new DVDList();
-		dvdlist.readFile();
-		dvdlist.printDVDNames();
-		
-	}
 
+
+
+}
 }
