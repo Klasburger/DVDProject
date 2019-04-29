a435838e77100248e032bce54d4e5a9d3793f81b
Andrew Snedeker <49925079+ansneds@users.noreply.github.com>
2019-04-29 17:47:50 -0500

    Update DriverMain.java

--- TeamProject/src/DriverMain.java
+++ TeamProject/src/DriverMain.java
@@ -2,9 +2,7 @@
 public class DriverMain {
 
 	public static void main(String[] args) {
-		//test toString()
-		DVDType test = new DVDType("The Fast and the Furious", "Paul Walker", "Neal H. Moritz", "Rob Cohen", "Universal Pictures", 5);
-		System.out.println(test);
+		
 		
 	}
 
