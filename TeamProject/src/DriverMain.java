30b437d055faac00eb83ec0ce345ec5d8c91d360
Kellogg23 <kellyaidan2@gmail.com>
2019-04-29 18:07:27 -0500

    main
    main

--- TeamProject/src/DriverMain.java
+++ TeamProject/src/DriverMain.java
@@ -7,7 +7,14 @@ public class DriverMain {
 		//test toString()
 		DVDType test = new DVDType("The Fast and the Furious", "Paul Walker", "Neal H. Moritz", "Rob Cohen", "Universal Pictures", 5);
 		System.out.println(test);
-
+		System.out.println("Select one of the following: \n");
+		System.out.println("1: To check wether the store carries a particular DVD. \n");
+		System.out.println("2: To check out a DVD. \n");
+		System.out.println("3: To check in a DVD. \n");
+		System.out.println("4: To check wether a particular DVD is in stock. \n");
+		System.out.println("5: To print only the titles of all the DVDS. \n");
+		System.out.println("6: To print a list of all DVDS. \n");
+		System.out.println("Enter choice: \n");
 
 
 }
