2d789d9f3be0364beee2b5fd1a545eb5446bad93
Klasburger <matthewklas@gmail.com>
2019-04-29 17:38:06 -0500

    New Class

---
+++ TeamProject/src/DVDList.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class DVDList {
+	
+	private ArrayList<DVDType> movieList=new ArrayList<DVDType>();
+	
+	DVDList(){
+		
+		
+		
+	}
+	
+	// method to read dvd text file and add the movies to the list
+	public void readFile() throws FileNotFoundException {
+		
+		File dvdFile=new File("DVD.txt");
+		Scanner inFile= new Scanner(dvdFile);
+		while(inFile.hasNextLine()) {
+			
+			DVDType tmpDVD= new DVDType();
+			tmpDVD.setMovie(inFile.nextLine());
+			tmpDVD.setStars(inFile.nextLine());
+			tmpDVD.setProducer(inFile.nextLine());
+			tmpDVD.setDirector(inFile.nextLine());
+			tmpDVD.setCompany(inFile.nextLine());
+			tmpDVD.setCopies(inFile.nextInt());
+			movieList.add(tmpDVD);
+			
+		}
+	
+		
+	}
+
+	//method to print the list of DVD names only
+	public void printDVDNames() {
+		
+		for(int i=0;i<movieList.size();++i) {
+			
+			movieList.get
+			
+		}
+		
+	}
+}
